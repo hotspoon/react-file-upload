9e0ed9882903ea9d5a1b18a39d99b585f84bb5bd
hotspoon <fariez18@outlook.com>
2021-09-27 22:13:12 +0700

    finishing js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {}
+import UploadFiles from "./components/upload-files.component";
+
+function App() {
+  return (
+    <div className="container" style={{ width: "600px" }}>
+      <div style={{ margin: "20px 0" }}>
+        <h3>wowalfarisi</h3>
+        <h4>React Drag & Drop File Upload example</h4>
+      </div>
+
+      <UploadFiles />
+    </div>
+  );
+}
 
 export default App;
--- src/components/upload-files.component.js
+++ src/components/upload-files.component.js
@@ -6,12 +6,14 @@ import UploadService from "../services/upload-files.service";
 export default class UploadFiles extends Component {
   constructor(props) {
     super(props);
+    this.upload = this.upload.bind(this);
+    this.onDrop = this.onDrop.bind(this);
+
     this.state = {
       selectedFiles: undefined,
       currentFile: undefined,
       progress: 0,
       message: "",
-
       fileInfos: [],
     };
   }
@@ -85,11 +87,33 @@ export default class UploadFiles extends Component {
             <section>
               <div {...getRootProps({ className: "dropzone" })}>
                 <input {...getInputProps()} />
-                {selectedFiles && selectedFiles[0].name ? <div className="selected-file">{}</div> : ""}
+                {selectedFiles && selectedFiles[0].name ? <div className="selected-file">{selectedFiles && selectedFiles[0].name}</div> : "Drag and drop file here, or click to select file"}
               </div>
+              <aside className="selected-file-wrapper">
+                <button className="btn btn-success" disabled={!selectedFiles} onClick={this.upload}>
+                  Upload
+                </button>
+              </aside>
             </section>
           )}
         </Dropzone>
+
+        <div className="alert alert-light" role="alert">
+          {message}
+        </div>
+
+        {fileInfos.length > 0 && (
+          <div className="card">
+            <div className="card-header">List of Files</div>
+            <ul className="list-group list-group-flush">
+              {fileInfos.map((file, index) => (
+                <li className="list-group-item" key={index}>
+                  <a href={file.url}>{file.name}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     );
   }
